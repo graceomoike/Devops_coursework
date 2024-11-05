5b0a80536891ac718e962b91cffe0f48d5a53f3c
graceomoike <graceomoikee@gmail.com>
2024-11-05 14:01:40 +0000

    string was added

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -19,7 +19,7 @@ public static int Arg1;
         }
 
         System.out.println("Hexadecimal representation is: " + hexadecimal);
-	System.out.println("Number has been converted!");
+	System.out.println("A Number has been converted!");
 
     }
 }
